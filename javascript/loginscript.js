619cd5ef425c92d6eeeb8b631c6050c400659316
c h e r i e <83512735+cherieeze@users.noreply.github.com>
2024-12-02 18:29:34 -0700

    login update

---
+++ javascript/loginscript.js
@@ -0,0 +1,29 @@
+const loginBtn = document.querySelector("#login");
+const registrationBtn = document.querySelector("#register");
+const loginForm = document.querySelector(".login-form");
+const registrationForm = document.querySelector(".registration-form");
+
+loginBtn.addEventListener('click', () => {
+  loginBtn.style.backgroundColor = '#02066F';
+  registrationBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+
+  loginForm.style.left = "50%";
+  registrationForm.style.left = "-50%";
+  loginForm.style.opacity = 1;
+  registrationForm.style.opacity = 0;
+});
+
+
+
+
+registrationBtn.addEventListener('click', () => {
+    loginBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+    registrationBtn.style.backgroundColor = '#02066F';
+
+
+    loginForm.style.left = "150%";
+    registrationForm.style.left = "50%";
+
+    loginForm.style.opacity = 0;
+    registrationForm.style.opacity = 1;
+  });
